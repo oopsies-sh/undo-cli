66da876bac668388681c3cc25b39a187c6c54089
Jason <chisanch@usc.edu>
2024-06-01 23:31:31 -0400

    initializing git tree

--- undo_cli/git.py
+++ undo_cli/git.py
@@ -1,12 +1,54 @@
 from utils import run_cmd
-import os
+from rich.console import Console
+from rich.syntax import Syntax
+
+
+"""
+Supported Git Commands: https://git-scm.com/docs
+"""
+
+
+# Internal git state machine to do simulations for Large Language Models to do retry Mechanism
+class Git:
+    def __init__(self):
+        self.log = None
+
+    @property
+    def get_log(self):
+        if not self.log:
+            self.fetch_log()
+        return self.log
+
+    def fetch_log(self):
+        cmd = ["git", "log", "--oneline", "--all"]
+        stdout, stderr = run_cmd(cmd)
+        if stdout:
+            self.log = stdout.decode("utf-8")
+        else:
+            self.log = "Error fetching logs" if stderr else "No logs available"
+
+
+# UI for Git Class
+class GitTree:
+    def __init__(self, git: Git, console: Console):
+        self.console = console
+        self.git = git
+
+    def display_log(self):
+        if self.git.get_log:
+            syntax = Syntax(
+                self.git.get_log, "bash", theme="monokai", line_numbers=True
+            )
+            self.console.print(syntax)
+        else:
+            self.console.print("No log available")
 
 
 def main():
-    log = run_cmd(["git", "log"])
-
-    if log:
-        print(log)
+    console = Console()
+    git = Git()
+    git_tree = GitTree(git, console)
+    git_tree.display_log()
 
 
 if __name__ == "__main__":
